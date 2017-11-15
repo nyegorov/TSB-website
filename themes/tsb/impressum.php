7d17f755e515e6fe6813618e58f413d7882333ab
nick <nick.yegorov@gmail.com>
2017-11-15 12:30:09 +0200

    Impressum position fix

--- themes/tsb/impressum.php
+++ themes/tsb/impressum.php
@@ -8,10 +8,7 @@ get_header(); ?>
 		jQuery('body').css('background-color', '#FF9F2C');
 		document.getElementById('tsb').style.display = 'none';
 		jQuery(window).load(function() {
-			jQuery('h1').css('color', '#000000');
-			jQuery('h1').css('padding-bottom', '5px');
 			jQuery('#menu_icon').attr("src", "<?php echo get_stylesheet_directory_uri();?>/icons/menu_black.svg");
-			jQuery('.site-content').css('padding-top', '80px'); 
 		});
 	</script>
 
@@ -21,7 +18,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) :
 			// Start the loop.
 			while ( have_posts() ) : the_post(); ?>
-			<div class="entry-content"><?php the_content(); ?></div>
+			<div class="entry-content impressum"><?php the_content(); ?></div>
 			<?php endwhile;
 		endif;
 		?>
